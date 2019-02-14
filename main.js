70553c3609268cabeda17c57a2ae4a37d82f0a6b
Dennis <oba.dennis@gmail.com>
2019-02-14 14:27:25 -0500

    Initial Code Test Implemented

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
-// alert("hello");
 
 
-// $(function(){
-//     alert("ready");
-// });
+
+
+
+const audio = $('#main-audio')[0];
+const mainSwitchStatus = () => {
+    let status = true;
+    $('.main-switch').on('click', function (e) {
+        e.preventDefault();
+        $('#trigger-1').toggleClass('on');
+        //audio.currentTime tip from Wes Bos
+        audio.currentTime = 0;
+        audio.play();
+        if (status === true) {
+            status = false
+        } else {
+            status = true;
+        }
+        console.log(status)
+    })
+
+}
+
+
+const sequenceOne = () => {
+    mainSwitchStatus();
+
+    
+
+    // $('.main-switch').on('click', function(){
+    //     console.log("clicked");
+    //     $('#trigger-1').toggleClass('on');
+    //     const mainSwitchStatus = {
+    //         status: true
+    //     }
+    //     mainSwitch.push(mainSwitchStatus);
+    //     console.log(mainSwitch[i]);
+    // }) 
+
+    // $('.switch-2').on('click', function(){
+    //     $('#trigger-2').toggleClass('on');
+    //     console.log("switch2");
+    // })
+    // $('.switch-3').on('click', function () {
+    //     $('#trigger-3').toggleClass('on');
+    // })
+}
+
+
+
+$(function(){
+    console.log("ready");
+    sequenceOne();
+})
