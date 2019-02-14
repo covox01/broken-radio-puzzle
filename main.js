4e14fdcd1ff7d8835958d6a94f7f1c607523dcb6
Dennis <oba.dennis@gmail.com>
2019-02-14 17:28:50 -0500

    Refined the Main Switch Code

--- main.js
+++ main.js
@@ -1,50 +1,44 @@
 
 
 
+const mainSwitchValue = (a) => {
+    console.log(a)
+}
 
-
-const audio = $('#main-audio')[0];
 const mainSwitchStatus = () => {
-    let status = true;
+    let trigger1 = {
+        name: 'Main Switch',
+        sequence: 0
+    }
+    const sequence = []
     $('.main-switch').on('click', function (e) {
         e.preventDefault();
         $('#trigger-1').toggleClass('on');
-        //audio.currentTime tip from Wes Bos
         audio.currentTime = 0;
         audio.play();
-        if (status === true) {
-            status = false
-        } else {
-            status = true;
+        // Inserts Key(Name) and Value(Name of Switch)
+        if (trigger1.sequence === 0) {
+            trigger1.sequence = 1;
+            sequence.push(trigger1);
+        } else if (trigger1.sequence === 1) {
+            trigger1.sequence = 2;
+        } else if (trigger1.sequence === 2) {
+            trigger1.sequence = 3;
+            trigger1.status = "Complete"
+        } else if (trigger1.sequence === 3) {
+            trigger1.sequence = 0;
+            sequence.length = 0;
         }
-        console.log(status)
+        mainSwitchValue(sequence);
     })
-
 }
 
+//assets
+const audio = $('#main-audio')[0];
 
 const sequenceOne = () => {
     mainSwitchStatus();
 
-    
-
-    // $('.main-switch').on('click', function(){
-    //     console.log("clicked");
-    //     $('#trigger-1').toggleClass('on');
-    //     const mainSwitchStatus = {
-    //         status: true
-    //     }
-    //     mainSwitch.push(mainSwitchStatus);
-    //     console.log(mainSwitch[i]);
-    // }) 
-
-    // $('.switch-2').on('click', function(){
-    //     $('#trigger-2').toggleClass('on');
-    //     console.log("switch2");
-    // })
-    // $('.switch-3').on('click', function () {
-    //     $('#trigger-3').toggleClass('on');
-    // })
 }
 
 
@@ -53,3 +47,29 @@ $(function(){
     console.log("ready");
     sequenceOne();
 })
+
+
+// const mainSwitchStatus = () => {
+//     let status = "start"
+//     const sequence = []
+//     $('.main-switch').on('click', function (e) {
+//         e.preventDefault();
+//         $('#trigger-1').toggleClass('on');
+//         audio.currentTime = 0;
+//         audio.play();
+//         if (sequence.length === 0) {
+//             status = "S1 Complete"
+//             sequence.push(status);
+//         } else if (status === "S1 Complete") {
+//             status = "S2 Complete";
+//             sequence.push(status);
+//         } else if (sequence.length === 2) {
+//             status = "S3 Success";
+//             sequence.push(status);
+//         } else if (status === "S3 Success") {
+//             status = "start"
+//             sequence.length = 0
+//         }
+//         mainSwitchValue(sequence);
+//     })
+// }
