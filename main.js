4ecb6e975faf4469e2acd44d211ab09521126941
Dennis <oba.dennis@gmail.com>
2019-02-15 16:44:17 -0500

    LOGIC AND AUDIO FOR SEQUENCE 1

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 const mainSwitch = () => {
     let switchProp = {
         name: 'Main Switch',
-        sequence: 0
+        sequence: 0,
+        complete: false
     }
     $('.main-switch').on('click', function (e) {
         e.preventDefault();
@@ -11,30 +12,30 @@ const mainSwitch = () => {
         // Inserts Key(Name) and Value(Name of Switch)
         if (switchProp.sequence === 0) {
             switchProp.sequence = 1;
-            switchProp.status = "Incomplete"
-            sequenceCheck.splice(0, 1, switchProp);
+            switchProp.complete = false;
+            sequenceCheck1.splice(0, 1, switchProp);
                 audioSwitch1.pause();
                 audioSwitch1.currentTime = 0;
                 audioSwitch1.play();
         } else if (switchProp.sequence === 1) {
             switchProp.sequence = 2;
-            switchProp.status = "Incomplete"
-            sequenceCheck.splice(0, 1, switchProp);
+            switchProp.complete = false;
+            sequenceCheck1.splice(0, 1, switchProp);
                 audioSwitch1.pause();
                 audioSwitch1.currentTime = 0;
                 audioSwitch1.play();
         } else if (switchProp.sequence === 2) {
             switchProp.sequence = 3;
-            switchProp.status = "Complete"
-            sequenceCheck.splice(0, 1, switchProp);
+            switchProp.complete = true;
+            sequenceCheck1.splice(0, 1, switchProp);
             $('#trigger-0').css('background', '#E78B2E');    
                 audioSwitch1.pause();
                 audioSwitch1.CurrentTime = 0;
                 audioSwitch2.play();
         } else if (switchProp.sequence === 3) {
-            switchProp.status = "Incomplete"
+            switchProp.complete = false;
             switchProp.sequence = 0;
-            sequenceCheck.splice(0, 1, switchProp);
+            sequenceCheck1.splice(0, 1, switchProp);
             $('#trigger-0').css('background', '#6B8983');  
                 audioSwitch2.pause();
                 audioSwitch2.currentTime = 0;
@@ -44,14 +45,14 @@ const mainSwitch = () => {
     })
 }
 const mainSwitchStatus = (status) => {
-    console.log(sequenceCheck);
+    console.log(sequenceCheck1);
 }
 //Gets Status of Switch One
 const switch1 = () => {
     let switchProp = {
         name: 'Switch 1',
         sequence: 0,
-        status: "Incomplete"
+        complete: false
     }   
     $('.switch-1').on('click', function (e) {
         e.preventDefault();
@@ -59,16 +60,16 @@ const switch1 = () => {
         if (switchProp.sequence === 0 || switchProp.sequence === 2) {
             switchProp.sequence = 0;
             switchProp.sequence = 1;
-            switchProp.status = "Incomplete";
-            sequenceCheck.splice(1, 1, switchProp);
+            switchProp.complete = false;
+            sequenceCheck1.splice(1, 1, switchProp);
             $('#trigger-1').css('background', '#6B8983');
                 audioSwitch2.pause();
                 audioSwitch2.currentTime = 0;
                 audioSwitch1.play();
         } else if (switchProp.sequence === 1) {
             switchProp.sequence = 2;
-            switchProp.status = "Complete";
-            sequenceCheck.splice(1, 1, switchProp);
+            switchProp.complete = true;
+            sequenceCheck1.splice(1, 1, switchProp);
             $('#trigger-1').css('background', '#E78B2E');  
                 audioSwitch1.pause();
                 audioSwitch1.currentTime = 0;
@@ -78,14 +79,15 @@ const switch1 = () => {
     })
 }
 const switchStatus1 = (status) => {
-    console.log(sequenceCheck);
+    console.log(sequenceCheck1);
 }
+
 //Get Status of Switch 2
 const switch2 = () => {
     let switchProp = {
         name: 'Switch 2',
         sequence: 0,
-        status: "Incomplete"
+        complete: false
     }
     $('.switch-2').on('click', function (e) {
         e.preventDefault();
@@ -93,16 +95,16 @@ const switch2 = () => {
         if (switchProp.sequence === 0 || switchProp.sequence === 2) {
             switchProp.sequence = 0;
             switchProp.sequence = 1;
-            switchProp.status = "Complete";
-            sequenceCheck.splice(2, 1, switchProp);
+            switchProp.complete = true;
+            sequenceCheck1.splice(2, 1, switchProp);
             $('#trigger-2').css('background', '#E78B2E');  
                 audioSwitch1.pause();
                 audioSwitch1.currentTime = 0;
                 audioSwitch2.play();
         } else if (switchProp.sequence === 1) {
             switchProp.sequence = 0;
-            switchProp.status = "Incomplete";
-            sequenceCheck.splice(2, 1, switchProp);
+            switchProp.complete = false;
+            sequenceCheck1.splice(2, 1, switchProp);
             $('#trigger-2').css('background', '#6B8983');
                 audioSwitch2.pause();
                 audioSwitch2.currentTime = 0;
@@ -112,70 +114,71 @@ const switch2 = () => {
     })
 }
 const switchStatus2 = (status) => {
-    console.log(sequenceCheck);
+    console.log(sequenceCheck1);
 }
-
 
 //Submit Sequence 1
 const submitSequence1 = () => {
-    const failCheck = {}
-    const success = []
+    let status = "check";
     $('.button').click( function(){
-        if (sequenceCheck[0].status === "Complete" && sequenceCheck[1].status === "Complete" && sequenceCheck[2].status === "Complete") {
+        if (sequenceCheck1[0].complete === true && sequenceCheck1[1].complete === true && sequenceCheck1[2].complete === true) {
             console.log("We Good");
+            status = true;
+            failCheck1.pop(status);
+            failCheck1.push(status)
+            console.log(failCheck1);
+        $('.success').fadeIn(400);
             audioBell.pause();
             audioBell.play();
         } else {
-            const fail = false;
-            console.log("we not good");
-            audioDud.pause();
-            audioDud.currentTime = 0;
-            audioDud.play();
-            $('.fail').fadeIn('ease');
-            success.push(fail);
-            console.log(success);
-            if (success[0] === fail) {
+            status = false;
+            console.log("We not good");
+            $('.success').hide();
+            $('.fail').fadeIn(300);
+            failCheck1.pop(status);
+            failCheck1.push(status);
+            console.log(failCheck1);
+                audioDud.pause();
+                audioDud.currentTime = 0;
+                audioDud.play();
+            if (failCheck1[0] === status) {
                 setTimeout(function(){
-                    $('.fail').fadeOut(1000);},
-                 1000);
-                
+                    $('.fail').fadeOut(300);},
+                800);
             }
-
         }
     })
 }
 
-// Test to see if sequence1 has been fulfilled
-const sequenceCheck = ["Main Switch", "Switch 1", "Switch 2"]
-
+// Test to see if sequence checks are true
+    const sequenceCheck1 = [1, 2, 3];
+    const failCheck1 = [];
 
 //Audio Name Space
-const audioSwitch1 = $('#audioSwitch-1')[0];
-const audioSwitch2 = $('#audioSwitch-2')[0];
-const audioSwitch3 = $('#audioSwitch-3')[0];
-const audioBell = $('#bell')[0];
-const audioDud = $('#dud')[0];
-
+    const audioSwitch1 = $('#audioSwitch-1')[0];
+    const audioSwitch2 = $('#audioSwitch-2')[0];
+    const audioSwitch3 = $('#audioSwitch-3')[0];
+    const audioBell = $('#bell')[0];
+    const audioDud = $('#dud')[0];
 
 //Sequence1
-const sequenceOne = () => {
-    mainSwitch();
-    switch1();
-    switch2();
-    //If submitSequence === True > Trigger Something
-    submitSequence1();
-}
+    const sequenceOne = () => {
+        mainSwitch();
+        switch1();
+        switch2();
+        submitSequence1();
+    }
 //Sequence1
-const sequenceTwo = () => {
-    switch1();
-    switch2();
-    switch3();
-    switch4();
-    switch5();
-    submitSequence2();
-}
+    const sequenceTwo = () => {
+        switch1();
+        switch2();
+        switch3();
+        switch4();
+        switch5();
+        submitSequence2();
+    }
 //Doc Ready
-$(function(){
-    console.log("ready");
-    sequenceOne();
-})
+    $(function(){
+        console.log("ready");
+        sequenceOne();
+    })
